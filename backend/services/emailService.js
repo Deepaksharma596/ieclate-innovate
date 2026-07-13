5ccb9ae72dda364e923aa02c1a2e4a8b27d03ea9
Deepaksharma596 <dipak6541.6615@gmail.com>
2026-07-13 22:55:10 +0530

    Update emailService.js

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -4,7 +4,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: Number(process.env.EMAIL_PORT),
-    secure: true,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
